082eef8069e1dfb574fd91a476ce0880831bbfda
Fernando Borin <fernandopborin@gmail.com>
2022-08-02 19:24:42 -0300

    python

--- Python/ex063.py
+++ Python/ex063.py
@@ -1,13 +1,17 @@
-termos = int(input('Quantos termos você quer mostrar? '))
+# Faz a sequência de Fibonacci até o termo digitado
+
+numtermos = int(input('Quantos termos você quer mostrar? '))
 count = 0
 fib1 = 0
 fib2 = 1
 
-while count < termos:
-    
+while count < numtermos - 2:
+    if count == 0:
+        print(fib1)
+        print(fib2)
     fib3 = fib1 + fib2
     print(fib3)
-    fib4 = fib3
-    fib2 = fib1
+    fib1 = fib2
+    fib2 = fib3
 
     count += 1
